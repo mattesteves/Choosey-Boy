16ef4735b814863f57a89bf8c1b2501f3461ad26
pigbog <mesteves@gmail.com>
2019-01-31 21:56:33 +0000

    added ability to add poll options

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,3 +8,25 @@ $(() => {
     }
   });;
 });
+
+
+let counter= 2;
+
+$( '.btn.btn-outline-secondary.start.add-option' ).click(function() {
+  event.preventDefault;
+  counter = counter+1;
+  $('.option-container').append(
+
+    `
+<div class="ind_option">
+Option ${counter}:<br>
+    <input class="form-control form-control-lg new_option" type="text" placeholder="Waffles">
+    <i class="fas fa-backspace"></i>  <br>
+</div>
+`
+  );
+});
+
+$('.fas.fa-backspace').click(function(){
+  $(this).closest('div').remove(); 
+});
