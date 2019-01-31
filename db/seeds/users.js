77b12488ab8fbcee66f723c35cd80ded2009f48b
vagrant <mikesnow444@gmail.com>
2019-01-31 00:48:56 +0000

    added migration that creates options table. added seeding for options table

--- db/seeds/users.js
+++ db/seeds/users.js
@@ -8,6 +8,10 @@ exports.seed = function(knex, Promise) {
     return knex('polls').del()
   }
 
+  function deleteOptions() {
+    return knex('options').del()
+  }
+
   function insertUsers() {
     return knex('users').insert([
       {id: 1, email: 'Alice@gmail.com'},
@@ -23,10 +27,25 @@ exports.seed = function(knex, Promise) {
       {id: 2, user_id: users[1].id, value: 'Favourite food'},
       {id: 3, user_id: users[2].id, value: 'Favourite colour'},
     ])
+    .returning('*')
   }
 
-  return deletePolls()
+  function insertOptions(polls) {
+    return knex('options').insert([
+      {id: 1, poll_id: polls[0].id, value: 'Airplane', description: 'Leslie Nielsen is great'},
+      {id: 2, poll_id: polls[0].id, value: 'The Fellowship of the Ring'},
+      {id: 3, poll_id: polls[1].id, value: 'Sushi'},
+      {id: 4, poll_id: polls[1].id, value: 'Sunflower Seeds'},
+      {id: 5, poll_id: polls[2].id, value: 'Orange', description: 'Clearly the winner'},
+      {id: 6, poll_id: polls[2].id, value: 'Blue'}
+    ])
+    .returning('*')
+  }
+
+  return deleteOptions()
+    .then(deletePolls)
     .then(deleteUsers)
     .then(insertUsers)
     .then(users => insertPolls(users))
+    .then(polls => insertOptions(polls))
 };
