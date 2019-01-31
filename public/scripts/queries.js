759897f2408df3f929beef5a71e71427fcfd496a
vagrant <mikesnow444@gmail.com>
2019-01-31 23:40:03 +0000

    added function that returns target value in a table with target id

--- public/scripts/queries.js
+++ public/scripts/queries.js
@@ -12,6 +12,11 @@ const knex = require('knex')({
   }
 })
 
+//disconnect from db
+function disconnect() {
+  knex.destroy();
+}
+
 // select user through email
 function selectUserByEmail(email){
   return(
@@ -20,51 +25,60 @@ function selectUserByEmail(email){
         email: email
       })
       .returning('*')
+      .catch(err => console.log('error selecting user by email', err))
   )
 }
 
 //inserts new user into users table
 function insertUser(email, selectUser) {
-  selectUser(email).then(user => {
-    if(!user[0]){
-      knex('users')
-        .insert({
-          email: email
-        })
-        .catch(err => console.log('error inputing user', err))
-    } else{
-      console.log('user in database')
-    }
-  })
+  return(
+    selectUser(email).then(user => {
+      if(!user[0]){
+        knex('users')
+          .insert({
+            email: email
+          })
+          .catch(err => console.log('error inputing user', err))
+      } else{
+        console.log('user in database');
+      }
+    })
+    .catch(err => console.log('error selecting user with input value', err))
+  )
 }
 
 // inserts new poll into polls table
 function insertPoll(email, value, options, descriptions, selectUser) {
-  selectUser(email).then(user => {
-      knex('polls')
-        .insert({
-          user_id: user[0].id, value: value
-        })
-        .returning('*')
-
-        //add inputed options to the poll
-        .then(poll => {
-          options.forEach((option, i) => {
-            insertOptions(poll, option, descriptions[i])
+  return(
+    selectUser(email).then(user => {
+      if(!user[0]) throw 'email not in database'
+        knex('polls')
+          .insert({
+            user_id: user[0].id, value: value
           })
-        })
-        .catch(err => console.log('error inserting poll', err))
-  })
+          .returning('*')
+
+          //add inputed options to the poll
+          .then(poll => {
+            options.forEach((option, i) => {
+              insertOptions(poll, option, descriptions[i])
+            })
+          })
+          .catch(err => console.log('error selecting user with input value', err))
+    })
+    .catch(err => console.log('error inputing user', err))
+  )
 }
 
 //inserts options for new poll
 function insertOptions(poll, option, description) {
-  console.log(poll, description)
-  knex('options')
-    .insert({
-      poll_id: poll[0].id, value: option, description: description
-    })
-    .catch(err => console.log('error inserting option', err))
+  return(
+    knex('options')
+      .insert({
+        poll_id: poll[0].id, value: option, description: description
+      })
+      .catch(err => console.log('error inserting option', err))
+  )
 }
 
 //selects option by target value row from options table
@@ -75,31 +89,57 @@ function selectOptionByValue(value) {
         value: value
       })
       .returning('*')
+      .catch(err => console.log('error selecting option', err))
   )
 }
 
 //inserts user votes into votes database
 function insertVotes(optionValue, userCookie, pointWeight, selectOption) {
-  selectOption(optionValue).then(option => {
-    knex('votes')
-      .insert({
-        option_id: option[0].id,
-        user_cookie: userCookie,
-        point_weight: pointWeight
-      })
-      .catch(err => console.log('error inserting vote', err))
-  })
+  return(
+    selectOption(optionValue).then(option => {
+      knex('votes')
+        .insert({
+          option_id: option[0].id,
+          user_cookie: userCookie,
+          point_weight: pointWeight
+        })
+        .catch(err => console.log('error inserting vote', err))
+    })
+    .catch(err => console.log('error selecting option with input value', err))
+  )
 }
 
-// insertVotes('Airplane', 12, 5, selectOptionByValue);
-// insertPoll('mike@gmail.com', 'poll1', ['option1', 'option2', 'option3'], ['desc1'], selectUserByEmail)
-// insertUser('mikesnow@gmail.com', selectUserByEmail);
+//return title of target poll
+async function returnFromDb1(pollId) {
+ try {
+  let selectPoll =  await knex.select('value').from('polls').where({id: pollId});
+  return selectPoll[0].value;
+  knex.destroy();
+ } catch(e) {
+  console.log('return poll title error');
+ }
+}
+
+//return value from target table
+async function getValue(table, returnValue, id) {
+ try {
+  let selectRow =  await knex.select(returnValue).from(table).where({id: id});
+  return selectRow[0][returnValue];
+ } catch(e) {
+  console.log('return poll title error');
+ }
+}
+
+//return sum of points for option
+
 
 module.exports = {
+  disconnect: disconnect,
   selectUserByEmail: selectUserByEmail,
   insertUser: insertUser,
   insertPoll: insertPoll,
   insertOptions: insertOptions,
   selectOptionByValue: selectOptionByValue,
-  insertVotes: insertVotes
+  insertVotes: insertVotes,
+  getValue: getValue
 }
---
+++ public/scripts/test.js
@@ -0,0 +1,9 @@
+const queries = require('./queries.js');
+
+
+// queries.insertUser('asdf@gmail.com', queries.selectUserByEmail);
+// queries.insertPoll('asdf@gmail.com', 'POLLLL', ['aaaa', 'bb', 'ccccc'], ['desc for aaa', '', 'desc for c'], queries.selectUserByEmail)
+
+// queries.insertVotes('Orange', 18, 4, queries.selectOptionByValue)
+
+
