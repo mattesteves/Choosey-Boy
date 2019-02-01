cdf8af8a9912133e0bbad21a6d6806e9d72fe2b0
vagrant <mikesnow444@gmail.com>
2019-02-01 16:18:56 +0000

    got rid of test file

--- public/scripts/test.js
+++
@@ -1,9 +0,0 @@
-const queries = require('./returnQueries.js');
-
-// queries.insertUser('aaaasddf@gmail.com')
-// queries.insertPoll('mike@gmail.com', 'NewPOOOLLLLL', ['aaasdfaa', 'ffbb', 'ccccc'], ['desc for aaa', '', 'desc for c'], queries.insertOptions)
-
-
-// queries.insertVotes('Orange', 18, 4, queries.selectOptionByValue)
-
-
