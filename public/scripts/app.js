e7efcacbb44473d0cb37a0c2e5d857773575417f
pigbog <mesteves@gmail.com>
2019-02-01 21:10:01 +0000

    Getting ready to merge

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -74,3 +74,19 @@ $('.pollshow_indoption').on('click', '.fas.fa-arrow-circle-down', function(){
 
 });
 
+
+  /* *******AJAX POST Request Event handler for email submit********* */
+  
+  $("#form123").click( function(event) {
+  
+   event.preventDefault();
+   const userEmail = $("#email123");
+   console.log("user email 3: ", userEmail);
+   if (userEmail.val() === "" || userEmail.val() === null) {
+     $(".error").text("Error ! Not a valid input.");
+   }else{
+        $.post( "/new_poll",userEmail);
+   }
+  
+  });
+  
